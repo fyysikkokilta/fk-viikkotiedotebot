cab259d98888aee00c0b06a77cf7b057b516e4e9
Elias-YJ <elias.yla-jarkko@live.fi>
2021-01-20 21:57:42 +0200

    Filter and format headers

--- data_processing.py
+++ data_processing.py
@@ -32,3 +32,31 @@ def next_week_news(base_url=weekly_base_url):
 
 def next_week_news_en():
     return next_week_news(weekly_base_url_en)
+
+
+def news_message_fi():
+    def filter_by_date(date):
+        event_date = datetime.date(date[2], date[1], date[0])
+        today = datetime.date.today()
+        return event_date - today < datetime.timedelta(days=8)
+
+    try:
+        data = current_news()["_default"].values()
+    except KeyError:
+        return "Tiedote on tyhjä"
+
+    headers = {"guild_soon": [], "guild": [], "other": []}
+    for event in data:
+        if (event["category"] == "Killan tapahtumat") & (filter_by_date(event["date"])):
+            headers["guild_soon"].append(
+                "<a>&#x23F0; {}</a>".format(event["header"])
+            )
+        elif event["category"] == "Killan tapahtumat":
+            headers["guild"].append(
+                "<a>&#x2022; {}</a>".format(event["header"])
+            )
+        else:
+            headers["other"].append(
+                "<a>&#x2022; {}</a>".format(event["header"])
+            )
+    print(headers)
