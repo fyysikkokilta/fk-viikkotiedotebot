164c535b9f7c817b6882353c0614eb5d0df125e2
Elias-YJ <elias.yla-jarkko@live.fi>
2021-01-20 20:28:33 +0200

    Hard code week numbers and urls

--- data_processing.py
+++ data_processing.py
@@ -3,9 +3,9 @@ import datetime
 from json.decoder import JSONDecodeError
 
 weekly_base_url = "https://www.fyysikkokilta.fi/wp-content/uploads/"\
-                     "viikkotiedote-data/{year}/{week}.json"
+                     "viikkotiedote-data/{week}.json"
 weekly_base_url_en = "https://www.fyysikkokilta.fi/wp-content/uploads/"\
-                     "viikkotiedote-data/{year}/{week}-en.json"
+                     "viikkotiedote-data/{week}-en.json"
 
 weekly_news_url = "https://www.fyysikkokilta.fi/viikkotiedote/"
 weekly_news_url_en = "https://www.fyysikkokilta.fi/en/viikkotiedote/"
@@ -13,7 +13,7 @@ weekly_news_url_en = "https://www.fyysikkokilta.fi/en/viikkotiedote/"
 
 def get_weekly_data(year, week_number, base_url=weekly_base_url):
     week_string = "week{:02}".format(week_number)
-    url = base_url.format(year=year, week=week_string)
+    url = base_url.format(week=week_string)
     try:
         data = requests.get(url).json()
     except JSONDecodeError:
@@ -24,7 +24,7 @@ def get_weekly_data(year, week_number, base_url=weekly_base_url):
 def current_news(base_url=weekly_base_url):
     today = datetime.date.today()
     year, week_number = today.isocalendar()[0:2]
-    return get_weekly_data(year, week_number, base_url)
+    return get_weekly_data(year, 47, base_url)
 
 
 def current_news_en():
@@ -35,7 +35,7 @@ def next_week_news(base_url=weekly_base_url):
     today = datetime.date.today()
     next_week = today + datetime.timedelta(days=7)
     year, week_number = next_week.isocalendar()[0:2]
-    return get_weekly_data(year, week_number, base_url)
+    return get_weekly_data(year, 48, base_url)
 
 
 def next_week_news_en():
@@ -45,7 +45,7 @@ def next_week_news_en():
 def news_message_fi(news_function):
     def filter_by_date(date):
         event_date = datetime.date(date[2], date[1], date[0])
-        today = datetime.date.today()
+        today = datetime.date(2020, 11, 16)
         return event_date - today < datetime.timedelta(days=8)
 
     try:
@@ -79,7 +79,7 @@ def news_message_fi(news_function):
 def news_message_en(news_function):
     def filter_by_date(date):
         event_date = datetime.date(date[2], date[1], date[0])
-        today = datetime.date.today()
+        today = datetime.date(2020, 11, 16)
         return event_date - today < datetime.timedelta(days=8)
 
     try:
